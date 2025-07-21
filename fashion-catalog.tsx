7b4f55dcad6064cd1d59617592b036504153e20d
Alo Esther <88891592+aloesther@users.noreply.github.com>
2025-07-21 20:11:23 +0100

    Update fashion-catalog.tsx

--- fashion-catalog.tsx
+++ fashion-catalog.tsx
@@ -1912,12 +1912,10 @@ Thank you!`
             <p>&copy; 2025 Jwela Brand. All rights reserved.</p>
           </div>
         </div>
-      </footer>
-    {/* Toast Notifications */}
-      <Toaster />
-    </div> {/* Closing outer div */}
-
-  );
-}
-
-export default FashionCatalog;
+          </footer>
+          
+          {/* Toast Notifications */}
+          <Toaster />
+        </div>
+      );
+    }
