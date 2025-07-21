fe172a58193ea2bdf5e92185acac959c0b59891c
Alo Esther <88891592+aloesther@users.noreply.github.com>
2025-07-21 18:45:43 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
